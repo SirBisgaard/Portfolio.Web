0973554d0d04db34a3d61aa527121d8174845669
Martin Erik Rieland Bisgaard <mebisgaard@gmail.com>
2020-08-05 12:56:40 +0200

    Added some content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Pages */
+import { HomeComponent } from './components/pages/home/home.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+
+/* Components */
+import { HeaderComponent } from './components/page-components/header/header.component';
+import { FooterComponent } from './components/page-components/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
